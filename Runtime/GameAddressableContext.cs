d05d79f01f44fb6fc22a657e253b8a64a748058f
josemgmz <jmgc92@gmail.com>
2026-03-04 17:42:58 +0100

    refactor: reduce addressable loading allocations

--- Runtime/GameAddressableContext.cs
+++ Runtime/GameAddressableContext.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -35,7 +34,7 @@ namespace UnityMVC
             var request = _LoadAssetsAsync<TObject>(keys);
             _requestHandles.Add(request);
             var result = await request.Task;
-            return instantiate ? result.Select(Object.Instantiate).ToList() : result.ToList();
+            return ToResultList(result, instantiate);
         }
 
         /// <summary>
@@ -47,8 +46,13 @@ namespace UnityMVC
         /// <returns>A task that represents the asynchronous operation. The task result contains the loaded asset.</returns>
         public async Task<TObject> LoadAssetAsync<TObject>(string key, bool instantiate = true) where TObject : Object
         {
-            var result = await LoadAssetsAsync<TObject>(new List<string> { key }, instantiate);
-            return result.FirstOrDefault();
+            var result = await LoadRawAssetAsync<TObject>(key);
+            if (!instantiate || result == null)
+            {
+                return result;
+            }
+
+            return Object.Instantiate(result);
         }
 
         /// <summary>
@@ -63,7 +67,7 @@ namespace UnityMVC
             var request = _LoadAssetsAsync<TObject>(keys);
             _requestHandles.Add(request);
             var result = request.WaitForCompletion();
-            return instantiate ? result.Select(Object.Instantiate).ToList() : result.ToList();
+            return ToResultList(result, instantiate);
         }
 
         /// <summary>
@@ -75,7 +79,13 @@ namespace UnityMVC
         /// <returns>The loaded asset.</returns>
         public TObject LoadAsset<TObject>(string key, bool instantiate = true) where TObject : Object
         {
-            return LoadAssets<TObject>(new List<string> { key }, instantiate).First();
+            var result = LoadRawAsset<TObject>(key);
+            if (!instantiate || result == null)
+            {
+                return result;
+            }
+
+            return Object.Instantiate(result);
         }
 
         /// <summary>
@@ -132,6 +142,19 @@ namespace UnityMVC
         private AsyncOperationHandle<IList<TObject>> _LoadAssetsAsync<TObject>(List<string> keys) where TObject : Object
         {
             return Addressables.LoadAssetsAsync<TObject>(keys, null, Addressables.MergeMode.Union, false);
+        }
+
+        private static List<TObject> ToResultList<TObject>(IList<TObject> result, bool instantiate) where TObject : Object
+        {
+            var count = result?.Count ?? 0;
+            var items = new List<TObject>(count);
+            for (var index = 0; index < count; index++)
+            {
+                var item = result[index];
+                items.Add(instantiate && item != null ? Object.Instantiate(item) : item);
+            }
+
+            return items;
         }
 
         #endregion
